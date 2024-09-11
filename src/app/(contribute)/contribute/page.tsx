8630febab1b376736ec02374c838762532b62913
abdout <osmanabdout@hotmail.com>
2024-09-11 12:22:42 +0300

    x

--- src/app/(contribute)/contribute/page.tsx
+++ src/app/(contribute)/contribute/page.tsx
@@ -1,60 +1,106 @@
-'use client';
-import Databayt from '@/components/atom/databayt'
-import React from 'react'
-import Image from 'next/image'
+import Title from '@/components/atom/databayt'
+import Block from '@/components/contribute/block'
+import Section from '@/components/contribute/section'
 import Link from 'next/link'
-import { Icon } from "@iconify/react";
+import React from 'react'
 
-const Contribute = () => {
+const contribute = () => {
   return (
-    <div className='flex flex-col   justify-center h-screen overflow-hidden' dir='ltr'>
-      <div className='flex flex-col items-start justify-start -mt-10'>
-        <Databayt title='NMBD' description='Contributors' />
-        <p className="text-[18px] font-light tracking-wider pl-1 pb-4 ">
-          To <strong>contribute</strong>, you may track the ongoing development by<br/>reviewing the Readme,
-          Code, Design and Discussion respectively 👇.
-        </p>
-        <div className="flex gap-6 items-start">
-          <Link href="/readme">
-          <Image
-            src="/contribute/readme.png"
-            alt="Readme"
-            width={70}
-            height={70}
-            className='reveal-less'
+    <div className='flex flex-col justify-center items-center py-4 md:py-8 md:px-4 mt-4'>
+      <div className='w-full max-w-screen-lg'>
+        <Title title='Contributors! 👋' description='Welcome' />
+        <div className='flex flex-col space-y-4 pt-1 '>
+          <p className='text-sm sm:text-base'>
+            Thank you for considering contributing to NMBD! Greatly appreciate your interest and support.
+            Below are some essential resources and information to help you <strong>get started</strong>:
+          </p>
+
+          <Section
+            title='Paradigm 🏷️'
+            body='The core principles and conceptual framework that guide NMBD.'
+            link='/tech-paradigm'
+            label='Link to Paradigm'
           />
-          </Link>
-         <Link href="https://github.com/abdout/nmbd">
-         <Image
-            src="/contribute/github.png"
-            alt="Github"
-            width={70}
-            height={70}
-            className='reveal-less'
+          <Section
+            title='Documentation 📚'
+            body='Before diving into the code, make sure to check out the documentation. It provides detailed explanations of project structure, guidelines, and how to contribute effectively.'
+            link='/docs'
+            label='Link to Documentation'
           />
-          </Link>
-          <Link href="https://www.figma.com/design/lNRkXcxd6EWe09goW2IMcn/nmbd?node-id=21-6&t=C9lVZynpKdkcNKZH-0">
-          <Icon
-            icon="file-icons:figma"
-            
-            width={60}
-            height={60}
-            className='reveal-less mt-1'
+          <Section
+            title='Repository 🏠'
+            body='Main repository serves as the central hub for the project. This is where you will find the core codebase and contribute to its development.'
+            link='https://github.com/abdout/nmbd'
+            label='Link to Repository'
           />
-          </Link>
-          <Link href="/readme">
-          <Image
-            src="/contribute/discord.png"
-            alt="Discord"
-            width={70}
-            height={70}
-            className='reveal-less'
+          <Section
+            title='Figma 🖌️'
+            body='Design repository for the project, containing wireframes, prototypes, and visual design assets.'
+            link='https://www.figma.com/file/your-file-id/your-file-name'
+            label='Link to Figma'
           />
-          </Link>
+          <Section
+            title='Localhost 💻'
+            body='A manual to set up a local development environment on your computer. This includes installing the necessary dependencies and configuring the necessary environment variables.'
+            link='/localhost'
+            label='Link to Localhost Manual'
+          />
+          <Section
+            title='Github 🐙'
+            body='A manual to master GitHub, including branch, issue, fork, clone, push, pull request, and merge.'
+            link='/github'
+            label='Link to Github Manual'
+          />
+          <Section
+            title='Pattern 🚊'
+            body='Tackle problems systematically using standardized patterns. This guide introduces you to a structured approach where you follow consistent steps for every problem, ensuring efficient and reliable solutions.'
+            link='/pattern'
+            label='Link to Pattern Manual'
+          />
+          <Section
+            title='Milestone 🎯'
+            body='Explore milestones to understand short-term goals and objectives. This section outlines key targets we aim to achieve in the near future.'
+            link='/milestone'
+            label='Link to Milestone'
+          />
+          <Section
+            title='Roadmap 🚀'
+            body='Discover long-term vision and planned features for NMBD. The roadmap provides an overview of strategic direction and future developments.'
+            link='/roadmap'
+            label='Link to Roadmap'
+          />
+          <Section
+            title='Building Blocks 🧱'
+            body='In addition to the main repository, we have child repositories dedicated to specific building blocks which serve as integral parts of the overall system. For more details, you may browse through the following building blocks:'
+          />
+          <Block />
+
+          <h4 className='pt-3 text-sm sm:text-lg'>Get in Touch 📬</h4>
+          <p className='text-sm sm:text-base'>
+            Have a suggestion, idea, question, feedback, or need assistance? Feel free to{' '}
+            <Link className='text-blue-700' href="https://github.com/abdout/databayt/issues">
+              open an issue
+            </Link>{' '}
+            or reach out on{' '}
+            <Link className='text-blue-700' href="">
+              Discord
+            </Link>.
+          </p>
+
+          <Section
+            title='Code of Conduct 🤝'
+            body='We maintain a code of conduct to ensure a welcoming and inclusive environment for all contributors. Please familiarize yourself with the code of conduct and adhere to its principles when interacting with the community.'
+            link='/conduct'
+            label='Link to Code of Conduct'
+          />
+          <Section
+            title='Thank You 🙏'
+            body='Once again, thank you for considering contributing to NMBD! Open source is the magic. ✨'
+          />
         </div>
       </div>
     </div>
   )
 }
 
-export default Contribute
+export default contribute
